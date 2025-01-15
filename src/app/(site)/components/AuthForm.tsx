5131b058b7c2df2168126c9e610a12486dd6598a
PramendraShukla1 <shuklapramendra99@gmail.com>
2025-01-15 15:05:42 +0530

    auth page setup complete

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import Input from "@/app/components/Input";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle, BsLinkedin } from "react-icons/bs";
 
 type Variant = "LOGIN" | "Register";
 
@@ -48,8 +51,76 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          <Input label="Email" />
+          {variant == "REGISTER" && (
+            <Input
+              id="email"
+              label="Email"
+              register={register}
+              errors={errors}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email Address"
+            register={register}
+            errors={errors}
+            type="email"
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            register={register}
+            errors={errors}
+            type="password"
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant == "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+            <AuthSocialButton
+              icon={BsLinkedin}
+              onClick={() => socialAction("linkedin")}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 text-gray-500">
+          <div>
+            {variant == "LOGIN"
+              ? "New to ChitChat ?"
+              : "Already have an account?"}
+          </div>
+          <div
+            className="underline cursor-pointer underline-offset-4"
+            onClick={toggleVariant}
+          >
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/components/inputs/Input.tsx
+++ src/app/components/inputs/Input.tsx
@@ -30,6 +30,19 @@ const Input: React.FC<InputProps> = ({
       >
         {label}
       </label>
+      <div className="mt-2">
+        <input
+          id={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus-within:ring-[#f5c032] sm:text-sm sm:leading-6`,
+            errors[id] && "focus:ring-red-600",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
     </div>
   );
 };
